cdaab50d9e576271d613403feb7b596e51625289
ANYAPESHKO <71599517+ANYAPESHKO@users.noreply.github.com>
2021-05-13 19:31:44 +0300

    Add files via upload

---
+++ Factorial/Factorial/Factorial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Factorial
+{
+    public static class Factorial
+    {
+        public static int Fact(int n)
+        {
+            if (n == 0) return 1;
+            else return n * Fact(n - 1);
+            
+        }
+
+        public static int Fibonachi(int x)
+        {
+            if (x == 0) return 0;
+            else if (x == 1) return 1;
+            else return Fibonachi(x - 1) + Fibonachi(x - 2);
+        }
+    }
+}
