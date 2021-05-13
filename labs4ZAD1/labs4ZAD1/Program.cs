2acb2244805b2d4c1234a5a682fbbbdf91ae5844
ANYAPESHKO <71599517+ANYAPESHKO@users.noreply.github.com>
2021-05-13 19:27:04 +0300

    Add files via upload

---
+++ labs4ZAD1/labs4ZAD1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+
+namespace labs4ZAD1
+{
+    class Program
+    {
+        [DllImport("User32.dll")]  static extern IntPtr GetDC(IntPtr hwnd);
+
+        [DllImport("User32.dll")] static extern int ReleaseDC(IntPtr hwnd, IntPtr dc);
+
+        static void Main(string[] args)
+        {
+            IntPtr desktop = GetDC(IntPtr.Zero);
+            using (Graphics g = Graphics.FromHdc(desktop))
+            {
+                while(true)
+                g.FillRectangle(Brushes.Blue, 0, 0, 400, 400);
+            }
+            ReleaseDC(IntPtr.Zero, desktop);
+          
+        }
+
+
+    }
+}
+
+
