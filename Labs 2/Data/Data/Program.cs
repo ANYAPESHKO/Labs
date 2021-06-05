9a192211695c2158feec5bf1d4cca9e3edae4e59
ANYAPESHKO <71599517+ANYAPESHKO@users.noreply.github.com>
2021-06-05 10:28:21 +0300

    Add files via upload

---
+++ Labs 2/Data/Data/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Data
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime now = DateTime.Now;
+            string str = now.ToString();
+            int[] count = new int[10];
+            Print(now);
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < str.Length; j++)
+                {
+                    if (str[j] == i + '0')
+                        count[i]++;
+                }
+            }
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine("Quantity of " + i + ": " + count[i]);
+
+            Console.ReadKey();
+        }
+        static void Print(DateTime now)
+        {
+            Console.Write("Time and date in first format:  ");
+            Console.WriteLine(now.ToString("ddd dd MMMM yyyy HH:mm:ss\n"));
+            Console.Write("Time and date in second format:  ");
+            Console.WriteLine(now.ToString("dd.MM.yyyy HH:mm:ss\n"));
+        }
+    }
+}
